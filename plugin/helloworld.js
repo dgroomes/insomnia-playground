1281d3dea97349227f941d760e7da6b5f21ed1f2
David Groomes <dwgroomes@gmail.com>
2022-04-03 16:18:43 -0500

    Add a "response hook" in the plugin

--- plugin/helloworld.js
+++ plugin/helloworld.js
@@ -1,3 +1,9 @@
+/*
+This is an Insomnia plugin.
+
+It adds a "Workspace action" button and a "response hook".
+*/
+
 console.log("[insomnia-playground/plugin] loaded")
 
 module.exports.workspaceActions = [{
@@ -7,4 +13,9 @@ module.exports.workspaceActions = [{
         context.app.alert("Hello World!",
             "This message brought to you by the 'insomnia-playground' project. See https://github.com/dgroomes/insomnia-playground.")
     },
+}];
+
+module.exports.responseHooks = [async ctx => {
+    const statusCode = ctx.response.getStatusCode();
+    console.log(`Hello from a 'response hook'! The status code is: ${statusCode}. This message brought to you by the 'insomnia-playground' project. See https://github.com/dgroomes/insomnia-playground.`)
 }];
